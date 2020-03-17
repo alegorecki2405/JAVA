d22cae6ff589d788f173c7e69d16c53bbc6053fc
Aleksander Gorecki <alegorecki@gmail.com>
2020-03-17 20:34:33 +0100

    just practicing

--- CodingPractice/src/DecimalComparator.java
+++ CodingPractice/src/DecimalComparator.java
@@ -1,9 +1,13 @@
 public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces(double a,double b){
+        a=a*1000;
+        b=b*1000;
 
-        if (Math.floor(a*1000)==Math.floor(b*1000)) {
-            System.out.println(Math.floor(a*1000));
-            System.out.println(Math.floor(b*1000));
+        int liczbaPierwsza = (int) a;
+        int liczbaDruga = (int) b;
+
+        if (liczbaDruga == liczbaPierwsza) {
+
             return true;
         }
         return false;
