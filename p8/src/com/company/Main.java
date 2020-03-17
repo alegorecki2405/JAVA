28add9793888ae6c0fd6ba6cdb9fa3032fa94699
Aleksander Gorecki <alegorecki@gmail.com>
2020-03-17 16:27:21 +0100

    just practice

---
+++ p8/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+public class PositiveNegativeZero{
+    public static void checkNumber(int Number){
+
+    }
+}
