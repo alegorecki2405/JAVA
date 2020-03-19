808ccd6b3f01d7b4334a6a5b227c62b953bede9c
Aleksander Gorecki <alegorecki@gmail.com>
2020-03-19 23:49:14 +0100

    overloading

--- p8/src/com/company/Main.java
+++ p8/src/com/company/Main.java
@@ -3,11 +3,38 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        calcFeetAndInchesToCentimeters(6,0);
+        calcFeetAndInchesToCentimeters(100);
     }
-}
-public class PositiveNegativeZero{
-    public static void checkNumber(int Number){
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player "+playerName+" scored :"+score+" points" );
+        return score*1000;
+    }
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored :"+score+" points" );
+        return score*1000;
+    }
+    public static int calculateScore(){
+        System.out.println("No player name , no play score" );
+        return 0;
+    }
 
+    public static double calcFeetAndInchesToCentimeters(double feet,double inches){
+        if(feet<0 || inches<0 || inches>12){
+            return -1;
+        }
+        double centimeters = (feet*12)*2.54+inches*2.54;
+        System.out.println(centimeters);
+        return centimeters;
     }
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if(inches<0){
+            return -1;
+        }
+        double feet = (int) inches/12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(feet+" and "+remainingInches);
+        return calcFeetAndInchesToCentimeters(feet,remainingInches);
+    }
+
 }
