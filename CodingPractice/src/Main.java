a8643432b58b2c9253ce4d246a3ae6d37854ba86
Aleksander Gorecki <alegorecki@gmail.com>
2020-03-24 18:37:18 +0100

    factor printer

--- CodingPractice/src/Main.java
+++ CodingPractice/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LastDigitChecker.hasSameLastDigit(41, 22, 71));
+        FactorPrinter.printFactors(4);
     }
 }
