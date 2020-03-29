416abb871ceee57968b39957650160b93b50f859
Aleksander Gorecki <alegorecki@gmail.com>
2020-03-29 21:06:32 +0200

    input calculator

--- CodingPractice/src/InputCalculator.java
+++ CodingPractice/src/InputCalculator.java
@@ -1,10 +1,27 @@
 import java.util.Scanner;
 
 public class InputCalculator {
-    Scanner scanner = new Scanner(System.in);
-    int suma = 0;
-    long srednia = 0;
-    int licznik = 0;
-    boolean hasAnInt = scanner.hasNextInt();
+    public static void inputThenPrintSumAndAverage()
+    {
+        int suma = 0;
+        int licznik = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
 
+            boolean hasAnInt = scanner.hasNextInt();
+            if(hasAnInt)
+            {
+                suma+=scanner.nextInt();
+                scanner.nextLine();
+                licznik+=1;
+            }
+            else
+            {
+                long srednia = suma/ licznik;
+                System.out.println("SUM = "+suma+" AVG = "+srednia);
+                break;
+            }
+        }
+    }
 }
--- CodingPractice/src/Main.java
+++ CodingPractice/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        MinAndMax.maxAndMin();
+        InputCalculator.InputThenPrintSumAndAverage();
     }
 }
