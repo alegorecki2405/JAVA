b8c5c1afe8434eb45223117f9d82d0c00cb7822c
Aleksander Gorecki <alegorecki@gmail.com>
2020-04-18 00:09:15 +0200

    polymorphism

--- codingpracitceoop/src/com/company/Main.java
+++ codingpracitceoop/src/com/company/Main.java
@@ -3,14 +3,102 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Printer printer = new Printer(true);
 
-        System.out.println(printer.getTonerLevel());
-        System.out.println(printer.getNumberOfPages());
-        printer.fillUp();
-        printer.printPage(50);
-        System.out.println(printer.getTonerLevel());
-        System.out.println(printer.getNumberOfPages());
+    }
+}
+class CarTwo{
+    private String name;
+    private boolean engine;
+    private int cylinders;
+    private int wheels;
 
+    public CarTwo(String name, int cylinders) {
+        this.name = name;
+        this.engine = true;
+        this.cylinders = cylinders;
+        this.wheels = 4;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void startEngine(){
+        System.out.println("the engine has started");
+
+    }
+    public void brake(){
+        System.out.println("the "+name+" is breaking");
+
+    }
+    public void accelerate(){
+        System.out.println("the "+name+" is getting faster");
+
+    }
+}
+class Lambo extends CarTwo{
+    public Lambo(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("roooooaaaaarrrr");;
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("iiiiiiihgghghghg");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("wruuuum");
+    }
+}
+
+class Mustang extends CarTwo{
+    public Mustang(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("rytytyytty");;
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("hhhhhhhh");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("hhmhmmhmhmhm");
+    }
+}
+
+class Porsche extends CarTwo{
+    public Porsche(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("łututututu");;
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("kszyszsszs");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("wrapapaaprapraprpa");
     }
 }
