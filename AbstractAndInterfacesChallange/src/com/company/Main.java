17f6f12a1251ef93204c4dacc0b667fea4024c4f
Aleksander Gorecki <alegorecki@gmail.com>
2020-05-05 23:16:19 +0200

    interfaces and abstract classes challange

--- AbstractAndInterfacesChallange/src/com/company/Main.java
+++ AbstractAndInterfacesChallange/src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         MyLinkedList list = new MyLinkedList(null);
         list.traverse(list.getRoot());
-        String stringData = "5 7 3";
-        String[] data = stringData.split("1 2 3");
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
+        String[] data = stringData.split(" ");
         for(String s : data){
             list.addItem(new Node(s));
         }
